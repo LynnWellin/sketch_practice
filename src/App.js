32796028d0fb9784f30912d8fa440a93727ccf86
Yury Lebedev <y.d.lebedev@gmail.com>
2019-12-17 13:20:27 -0500

    Style update

--- src/App.js
+++ src/App.js
@@ -5,53 +5,21 @@ import TextCombinations from './components/TextCombinations';
 import GridSystemCont from './components/GridSystemCont';
 import Typography from './components/Typography';
 import { theme } from './theme/theme';
-import './index.css';
-// require('/fonts');
 // Import fonts to ensure they are bundled during build
-// import './fonts/Inter/Inter-Bold.woff';
-// import './fonts/Inter/Inter-Bold.woff2';
 // import './fonts/Inter/Inter-Regular.woff';
 // import './fonts/Inter/Inter-Regular.woff2';
 // import './fonts/Inter/Inter-SemiBold.woff';
 // import './fonts/Inter/Inter-SemiBold.woff2';
+// import './fonts/Inter/Inter-Bold.woff';
+// import './fonts/Inter/Inter-Bold.woff2';
 
 const useStyles = makeStyles({
     root: {
-        // '@font-face': {
-        //     'font-family': 'Inter - Semi Bold',
-        //     'font-style': 'normal',
-        //     'font-weight': 600,
-        //     'font-display': 'swap',
-        //     src:
-        //         'local(Inter), url(./fonts/Inter/Inter-SemiBold.woff2) format("woff2"),' +
-        //         'url(./fonts/Inter/Inter-SemiBold.woff?v=3.11) format("woff")',
-        // },
-
-        // '@font-face': {
-        //     'font-family': 'Inter - Bold',
-        //     'font-style': 'normal',
-        //     'font-weight': 700,
-        //     'font-display': 'swap',
-        //     src:
-        //         'local(Inter), url("./fonts/Inter/Inter-Bold.woff2?v=3.11") format("woff2"),' +
-        //         'url("./fonts/Inter/Inter-Bold.woff?v=3.11") format("woff")',
-        // },
-
-        // '@font-face': {
-        //     'font-family': 'Inter - Regular',
-        //     'font-style': 'normal',
-        //     'font-weight': '400',
-        //     'font-display': 'swap',
-        //     src:
-        //         'url(./fonts/Inter/Inter-Regular.woff2?v=3.11) format("woff2"),' +
-        //         'url(./fonts/Inter/Inter-Regular.woff?v=3.11) format("woff")',
-        // },
         maxWidth: '1044px',
         margin: 0,
         padding: 0,
         color: theme.palette.gray,
         '& h2': {
-            fontFamily: '"Inter - Semi Bold"',
             padding: 0,
             margin: 0,
             fontSize: '17px',
